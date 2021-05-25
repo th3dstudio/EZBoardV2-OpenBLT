3f04abbd29f3d225903d540bc8d24e00d1833064
Tim Hoogland <tim@th3dstudio.com>
2021-05-24 22:08:19 -0500

    Test1

--- src/Config.h
+++ src/Config.h
@@ -1,18 +1,3 @@
-
-
-
-
-
-// Nano v3
-// #define HSE_VALUE 8000000
-// #define SDCARD_SS_PIN                       PC9
-// #define SDCARD_SCK_PIN                      PC10
-// #define SDCARD_MISO_PIN                     PC11
-// #define SDCARD_MOSI_PIN                     PC12
-// #define SD_DETECT_PIN                       PD12
-// #define SPI_SPEED                           2
-
-
 #define HSE_VALUE 12000000
 #define SDCARD_SS_PIN                       PA4
 #define SDCARD_SCK_PIN                      PA5
@@ -21,5 +6,5 @@
 #define SD_DETECT_PIN                       -1
 #define SPI_SPEED                           2
 
-#define FIRMWARE_FILENAME                   "firmware.srec"
+#define FIRMWARE_FILENAME                   "firmware.bin"
 #define FIRMWARE_FILENAME_RENAME            "firmware.cur"
